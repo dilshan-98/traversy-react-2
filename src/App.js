ebc56050d844144677c18cdd26a0959f86e4e972
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-04-18 01:23:45 +0530

    add toggle reminder from BE

--- src/App.js
+++ src/App.js
@@ -15,19 +15,56 @@ function App() {
 
   //2nd ---> Delete from the server using HTTP request
   const deleteHandler = async (id) => {
-   await fetch(`http://localhost:5000/tasks/${id}`, {
-     method: "DELETE"
-   })
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE"
+    })
   }
 
   //toggle reminder for the tasks
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) =>
-      task.id !== id ? task : {
-        ...task,
-        reminder: !task.reminder
-      })
-    )
+  //1st ---> to toggle reminder in the static data
+  // const toggleReminder = (id) => {
+  //   setTasks(tasks.map((task) =>
+  //     task.id !== id ? task : {
+  //       ...task,
+  //       reminder: !task.reminder
+  //     })
+  //   )
+  // }
+
+  //2nd ---> toggle reminder in the server data
+  const fetchTask = async (id) => {
+    const response = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await response.json();
+
+    return data;
+  }
+
+  const toggleReminder = async (id) => {
+    const taskFetched = await fetchTask(id);
+    const updatedTask = {
+      ...taskFetched,
+      reminder: !taskFetched.reminder
+    }
+
+    const response = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(updatedTask)
+    });
+
+    const data = await response.json();
+
+    //This is not necessary when data is fetched from BE in useEffect where the changing values were added as [tasks]
+    setTasks(tasks.map((task) => task.id !== id ? 
+    task :
+    {
+      ...task,
+      reminder: data.reminder
+    }
+    ))
+
   }
 
   //Add Task
@@ -71,14 +108,14 @@ function App() {
   // })
 
   //2nd method
-  useEffect (() => {
+  useEffect(() => {
     const getTasks = async () => {
       const tasksFromServer = await fetchTasks();
       setTasks(tasksFromServer);
     }
 
     getTasks();
-  })
+  }, [])
 
   const fetchTasks = async () => {
     const response = await fetch("http://localhost:5000/tasks");
