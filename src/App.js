b25acd65099616424d32d43a9a8fa1452323189c
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-09-01 21:31:28 +0530

    Moved the tasks array to App where we used props to move it back to Tasks.js using useState.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 const App = () => {
+  const [tasks, useTasks] = useState(
+    [
+      {
+        id: 1,
+        text: "Doctor's Appointment",
+        day: "Jan 10th at 5.00PM",
+        reminder: true
+      },
+      {
+        id: 2,
+        text: "Teacher's Appointment",
+        day: "Mar 10th at 5.00PM",
+        reminder: false
+      },
+      {
+        id: 9,
+        text: "Seller's Appointment",
+        day: "Feb 10th at 5.00PM",
+        reminder: true
+      },
+      {
+        id: 8,
+        text: "Lawyer's Appointment",
+        day: "Feb 17th at 5.00PM",
+        reminder: false
+      }
+    ]
+  );
+
   return (
     <div className="App">
-      <Header title="Tasks Tracker"/>
-      <Tasks />
+      <Header title="Tasks Tracker" />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
